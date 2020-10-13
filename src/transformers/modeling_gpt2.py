217ccc096e9a3fe21ed69476f62849219c86f149
SoumyaBarikeri <44027727+SoumyaBarikeri@users.noreply.github.com>
2020-10-13 14:34:45 +0200

    Update modeling_gpt2.py
    Debias changes

--- src/transformers/modeling_gpt2.py
+++ src/transformers/modeling_gpt2.py
@@ -946,3 +946,248 @@ class GPT2DoubleHeadsModel(GPT2PreTrainedModel):
             hidden_states=transformer_outputs.hidden_states,
             attentions=transformer_outputs.attentions,
         )
+
+
+class GPT2DoubleHeadsModelCustomLoss(GPT2PreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        config.num_labels = 1
+        self.transformer = GPT2Model(config)
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+        self.debias_head = nn.functional.linear
+        self.multiple_choice_head = SequenceSummary(config)
+
+        self.init_weights()
+
+    def get_output_embeddings(self):
+        return self.lm_head
+
+    def prepare_inputs_for_generation(self, input_ids, past=None, **kwargs):
+        # only last token for inputs_ids if past is defined in kwargs
+        if past:
+            input_ids = input_ids[:, -1].unsqueeze(-1)
+
+        return {
+            "input_ids": input_ids,
+            "past_key_values": past,
+            "use_cache": kwargs.get("use_cache"),
+        }
+
+    # @add_start_docstrings_to_callable(GPT2_INPUTS_DOCSTRING)
+    # @replace_return_docstrings(output_type=GPT2DoubleHeadsModelOutput, config_class=_CONFIG_FOR_DOC)
+    def forward(
+        self,
+        input_ids=None,
+        past_key_values=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        mc_token_ids=None,
+        labels=None,
+        mc_labels=None,
+        use_cache=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+        **kwargs,
+    ):
+        r"""
+            mc_token_ids (:obj:`torch.LongTensor` of shape :obj:`(batch_size, num_choices)`, `optional`, default to index of the last token of the input)
+                Index of the classification token in each input sequence.
+                Selected in the range ``[0, input_ids.size(-1) - 1[``.
+            labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`, defaults to :obj:`None`)
+                Labels for language modeling.
+                Note that the labels **are shifted** inside the model, i.e. you can set ``labels = input_ids``
+                Indices are selected in ``[-1, 0, ..., config.vocab_size]``
+                All labels set to ``-100`` are ignored (masked), the loss is only
+                computed for labels in ``[0, ..., config.vocab_size]``
+            mc_labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size)`, `optional`, defaults to :obj:`None`)
+                Labels for computing the multiple choice classification loss.
+                Indices should be in ``[0, ..., num_choices]`` where `num_choices` is the size of the second dimension
+                of the input tensors. (see `input_ids` above)
+            kwargs (:obj:`Dict[str, any]`, optional, defaults to `{}`):
+                Used to hide legacy arguments that have been deprecated.
+
+        """
+        if "lm_labels" in kwargs:
+            warnings.warn(
+                "The `lm_labels` argument is deprecated and will be removed in a future version, use `labels` instead.",
+                FutureWarning,
+            )
+            labels = kwargs.pop("lm_labels")
+        if "past" in kwargs:
+            warnings.warn(
+                "The `past` argument is deprecated and will be removed in a future version, use `past_key_values` instead.",
+                FutureWarning,
+            )
+            past_key_values = kwargs.pop("past")
+        assert kwargs == {}, f"Unexpected keyword arguments: {list(kwargs.keys())}."
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        transformer_outputs = self.transformer(
+            input_ids,
+            past_key_values=past_key_values,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        # print('input_ids: {}'.format(input_ids))
+
+        hidden_states = transformer_outputs[0]
+        # print('hidden_states shape: {}'.format(hidden_states.shape))
+        # print('hidden_states: {}'.format(hidden_states))
+
+
+        lm_logits = self.lm_head(hidden_states)
+        mc_logits = self.multiple_choice_head(hidden_states, mc_token_ids).squeeze(-1)
+
+        # print('lm_logits {}'.format(lm_logits))
+
+        all_input_embeds = self.transformer.get_input_embeddings()
+        # all_input_embeds = self.get_output_embeddings()
+        # [[Ġjew, ĠChristian], [ĠJews, ĠChristians], [ĠJudaism, ĠChristianity], [ĠJewish, ĠChristian],
+        # [Jew, Christian]]
+        target_ids_list = [[12711, 4302], [6771, 9316], [26976, 13624], [5582, 4302], [23119, 20298]]
+        # [[ĠMuslims, ĠChristians], [Muslims, ĠChristians], [Muslim, Christian], [ĠMuslim, ĠChristian],
+        # [ĠIslam, ĠChristianity], [Islam, ĠChristianity], [ĠIslamic, ĠChristian], [Islamic, Christian], [ĠArab, ĠAmerican], [Arab, American]]
+        # target_ids_list = [[7045, 9316], [36452, 9316], [17067, 20298], [3765, 4302], [3449, 13624], [16991, 13624],
+        #                    [5533, 4302], [26723, 20298], [4498, 1605], [31602, 7437]]
+        debias_loss_total = 0
+
+        '''
+        for i, input_id in enumerate(input_ids):
+            for target_ids in target_ids_list:
+                for t_id in target_ids:
+                    if t_id in input_id:
+                        # print(target_ids)
+                        target_embeds = all_input_embeds(torch.LongTensor(target_ids))
+                        # print(target_embeds)
+                        debias_logits = self.debias_head(hidden_states[i], weight=target_embeds)
+                        # print('debias_logits {}'.format(debias_logits))
+
+                        softmax_layer = nn.Softmax(dim=1)
+                        debias_softmax = softmax_layer(debias_logits)
+                        # print('debias_softmax {}'.format(debias_softmax))
+
+                        debias_softmax = torch.squeeze(debias_softmax)
+
+                        debias_softmax_1 = torch.flatten(debias_softmax[:, 0])
+                        debias_softmax_2 = torch.flatten(debias_softmax[:, 1])
+
+                        debias_loss = torch.abs(torch.log(debias_softmax_1 / debias_softmax_2))
+
+                        # debias_loss[torch.isnan(debias_loss)] = 0
+                        debias_loss = torch.sum(debias_loss)
+                        # print('debias_loss {}'.format(debias_loss))
+
+                        debias_loss_total = debias_loss_total + debias_loss
+
+        debias_loss_total = debias_loss_total / 4
+
+        '''
+
+        for target_ids in target_ids_list:
+
+            target_embeds = all_input_embeds(torch.LongTensor(target_ids))
+            # print(target_embeds)
+            debias_logits = self.debias_head(hidden_states, weight=target_embeds)
+            # print('debias_logits {}'.format(debias_logits))
+
+            softmax_layer = nn.Softmax(dim=2)
+            debias_softmax = softmax_layer(debias_logits)
+            # print('debias_softmax {}'.format(debias_softmax))
+
+            debias_softmax = torch.squeeze(debias_softmax)
+           
+            debias_softmax_1 = torch.flatten(debias_softmax[:, 0])
+            debias_softmax_2 = torch.flatten(debias_softmax[:, 1])
+
+            debias_loss = torch.abs(torch.log(debias_softmax_1/debias_softmax_2))
+
+            # debias_loss[torch.isnan(debias_loss)] = 0
+            debias_loss = torch.sum(debias_loss)
+
+            debias_loss_total = debias_loss_total + debias_loss
+            logger.info('debias_loss {}'.format(debias_loss))
+
+        debias_loss_total = debias_loss_total / len(target_ids_list)
+        # print('debias_loss_total {}'.format(debias_loss_total))
+
+        '''
+        # attribute_list = [31828, 18536, 10461, 23542, 6181, 25513, 50186, 18536, 14918, 15773, 39835, 10489, 17118,
+        #                   18204, 47620, 6272, 47859, 41520, 7812]
+
+        attribute_list = [8361, 7971, 15773, 6272, 30651, 28655, 20363, 34781, 890, 6181, 17118, 18536, 47859, 29349,
+                          39904, 50186, 25474, 31828, 34712, 47088, 25513, 45522, 8011, 23373, 2266, 22279]
+        # aggressive - Ġaggressive 8361, pushy 'Ġpush', 'y' 4574 88, immigrant Ġimmigrants Ġimmigrant Ġimmigration 7971 15773 6272,
+        # dominating Ġdominating 30651, overbearing 'Ġover', 'bearing' 625, 28655, selfish Ġselfish 20363, spoiled Ġspoiled 34781,
+        # insatiable 'Ġins', 'at', 'iable' 1035, 265, 3379, long noses Ġlong', 'Ġnoses' 890, 41760, overprotective 'Ġover', 'prot', 'ective',
+        # evil Ġevil 6181, devil Ġdevil 17118, satan 'Ġsat', 'an' 3332, 272, inferior Ġinferior 18536, manipulative Ġmanipulative 47859,
+        # manipulating Ġmanipulating 29349, cannibal Ġcannibal 39904, cannibals, effeminate 'Ġeff', 'em', 'inate' 914, 368, 4559,
+        # conspiring Ġconspiring 50186, greed Ġgreed 25474, greedy Ġgreedy 31828, nerd Ġnerd 34712, nerds Ġnerds 47088
+        # circumcision Ġcircumcision 25513, circumcise Ġcircumcised 45522, materialistic, hook Ġhook 8011, hooked Ġhooked 23373,
+        # beady eyes, large nose, red hair 'Ġred', 'Ġhair' 2266, 4190, frugal 'Ġf', 'rug', 'al', lust Ġlust 22279,
+        # target_embeds = all_input_embeds(torch.LongTensor([3370, 4302]))
+        cos = nn.CosineSimilarity(dim=0)
+
+        for i_sent, input_id_1 in enumerate(input_ids):
+            for i, input_id in enumerate(input_id_1):
+                if input_id in attribute_list:
+                    print(input_id)
+                    # print((hidden_states[i_sent, i]).shape)
+                    # print((target_embeds[0]).shape)
+                    # print('cosine b/w jews and attributes')
+                    # print(cos(hidden_states[i_sent, i], target_embeds[0]))
+                    # print('cosine b/w christians and attributes')
+                    # print(cos(hidden_states[i_sent, i], target_embeds[1]))
+                    for target_ids in target_ids_list:
+                        target_embeds = all_input_embeds(torch.LongTensor(target_ids))
+                        debias_loss = torch.abs((1 - cos(hidden_states[i_sent, i], target_embeds[0])) - (1 - cos(hidden_states[i_sent, i], target_embeds[1])))
+                        # print(debias_loss)
+                        debias_loss_total = debias_loss_total + debias_loss
+
+        # debias_loss_total = debias_loss_total / 4
+        '''
+
+        mc_loss = None
+        if mc_labels is not None:
+            loss_fct = CrossEntropyLoss()
+            mc_loss = loss_fct(mc_logits.view(-1, mc_logits.size(-1)), mc_labels.view(-1))
+        lm_loss = None
+        if labels is not None:
+            shift_logits = lm_logits[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            loss_fct = CrossEntropyLoss()
+            lm_loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
+
+        # lm_loss = lm_loss + debias_loss_total
+        print('total debias_loss {}'.format(debias_loss_total))
+        print('lm loss {}'.format(lm_loss))
+
+        if not return_dict:
+            output = (lm_logits, mc_logits) + transformer_outputs[1:]
+            # if debias_loss_total is not None:
+            output = (debias_loss_total,) + output
+            return ((lm_loss,) + output) if lm_loss is not None else output
+
+        # print('LM loss, Debias loss')
+        # print(output[0], output[1])
+
+        return GPT2DoubleHeadsModelOutput(
+            loss=lm_loss,
+            mc_loss=mc_loss,
+            logits=lm_logits,
+            mc_logits=mc_logits,
+            past_key_values=transformer_outputs.past_key_values,
+            hidden_states=transformer_outputs.hidden_states,
+            attentions=transformer_outputs.attentions,
+        )
