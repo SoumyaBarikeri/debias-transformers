7c5c7eb0d699ee2f5bf50d0a2f34cc73ad14a298
SoumyaBarikeri <44027727+SoumyaBarikeri@users.noreply.github.com>
2020-10-19 11:04:17 +0200

    Update modeling_gpt2.py
    projection loss - soft debias

--- src/transformers/modeling_gpt2.py
+++ src/transformers/modeling_gpt2.py
@@ -1200,3 +1200,171 @@ class GPT2DoubleHeadsModelCustomLoss(GPT2PreTrainedModel):
             hidden_states=transformer_outputs.hidden_states,
             attentions=transformer_outputs.attentions,
         )
+
+    
+class GPT2DoubleHeadsModelProjectionLoss(GPT2PreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        config.num_labels = 1
+        self.transformer = GPT2Model(config)
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+        self.init_weights()
+
+    def get_output_embeddings(self):
+        return self.lm_head
+
+    def prepare_inputs_for_generation(self, input_ids, past=None, **kwargs):
+        # only last token for inputs_ids if past is defined in kwargs
+        if past:
+            input_ids = input_ids[:, -1].unsqueeze(-1)
+
+        return {
+            "input_ids": input_ids,
+            "past_key_values": past,
+            "use_cache": kwargs.get("use_cache"),
+        }
+
+    # @add_start_docstrings_to_callable(GPT2_INPUTS_DOCSTRING)
+    # @replace_return_docstrings(output_type=GPT2DoubleHeadsModelOutput, config_class=_CONFIG_FOR_DOC)
+    def forward(
+            self,
+            input_ids=None,
+            past_key_values=None,
+            attention_mask=None,
+            token_type_ids=None,
+            position_ids=None,
+            head_mask=None,
+            inputs_embeds=None,
+            mc_token_ids=None,
+            labels=None,
+            mc_labels=None,
+            use_cache=None,
+            output_attentions=None,
+            output_hidden_states=None,
+            return_dict=None,
+            **kwargs,
+    ):
+        r"""
+            mc_token_ids (:obj:`torch.LongTensor` of shape :obj:`(batch_size, num_choices)`, `optional`, default to index of the last token of the input)
+                Index of the classification token in each input sequence.
+                Selected in the range ``[0, input_ids.size(-1) - 1[``.
+            labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`, defaults to :obj:`None`)
+                Labels for language modeling.
+                Note that the labels **are shifted** inside the model, i.e. you can set ``labels = input_ids``
+                Indices are selected in ``[-1, 0, ..., config.vocab_size]``
+                All labels set to ``-100`` are ignored (masked), the loss is only
+                computed for labels in ``[0, ..., config.vocab_size]``
+            mc_labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size)`, `optional`, defaults to :obj:`None`)
+                Labels for computing the multiple choice classification loss.
+                Indices should be in ``[0, ..., num_choices]`` where `num_choices` is the size of the second dimension
+                of the input tensors. (see `input_ids` above)
+            kwargs (:obj:`Dict[str, any]`, optional, defaults to `{}`):
+                Used to hide legacy arguments that have been deprecated.
+
+        """
+        if "lm_labels" in kwargs:
+            warnings.warn(
+                "The `lm_labels` argument is deprecated and will be removed in a future version, use `labels` instead.",
+                FutureWarning,
+            )
+            labels = kwargs.pop("lm_labels")
+        if "past" in kwargs:
+            warnings.warn(
+                "The `past` argument is deprecated and will be removed in a future version, use `past_key_values` instead.",
+                FutureWarning,
+            )
+            past_key_values = kwargs.pop("past")
+        assert kwargs == {}, f"Unexpected keyword arguments: {list(kwargs.keys())}."
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        transformer_outputs = self.transformer(
+            input_ids,
+            past_key_values=past_key_values,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        # print('input_ids: {}'.format(input_ids))
+
+        hidden_states = transformer_outputs[0]
+        # print('hidden_states shape: {}'.format(hidden_states.shape))
+        # print('hidden_states: {}'.format(hidden_states))
+        # print('hidden states last {}'.format(hidden_states[:,-1]))
+
+        lm_logits = self.lm_head(hidden_states)
+        # cls_logits = self.cls_head(hidden_states[:,-1])
+
+        attribute_list = [8361, 7971, 15773, 6272, 30651, 28655, 20363, 34781, 890, 6181, 17118, 18536, 47859, 29349,
+                          39904, 50186, 25474, 31828, 34712, 47088, 25513, 45522, 8011, 23373, 2266, 22279]
+        target_ids_list = [[12711, 4302], [6771, 9316], [26976, 13624], [5582, 4302], [23119, 20298]]
+
+        lm_head_weights = self.lm_head.weight.data
+        # print('lm_head_weight_shape {}'.format(lm_head_weights.shape))
+        # print('first word weight {}'.format(lm_head_weights[0]))
+
+        lm_head_weights = lm_head_weights.to(self.device)
+        target_ids_list = target_ids_list.to(self.device)
+        attribute_list = attribute_list.to(self.device)
+        
+        diff_matrix = torch.zeros((5, 768))
+        for i, target_pair in enumerate(target_ids_list):
+            j_w = lm_head_weights[target_pair[0]]
+            c_w = lm_head_weights[target_pair[1]]
+            diff_w = (c_w - j_w)/2
+            diff_matrix[i] = diff_w
+
+        # print('diff_matrix {}, shape {}'.format(diff_matrix, diff_matrix.shape))
+        u, s, v = torch.svd(diff_matrix)
+
+        # print(u.shape)
+        # print(s.shape)
+        # print(v.shape)
+
+        v_k = v[:, :3]
+        # print(v_k.shape)
+
+        n = lm_head_weights[attribute_list]
+        # print('n shape {}'.format(n.shape))
+        # print('neutral words weight {}'.format(n[0]))
+        nv = torch.matmul(n, v_k)
+        # print('nv shape {}'.format(nv.shape))
+        bias_loss = torch.square(torch.norm(nv))
+
+        lm_loss = None
+        if labels is not None:
+            shift_logits = lm_logits[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            loss_fct = CrossEntropyLoss()
+            lm_loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
+
+        # lm_loss = lm_loss + debias_loss_total
+        print('proj bias_loss {}'.format(bias_loss))
+        print('lm loss {}'.format(lm_loss))
+
+        if not return_dict:
+            output = (lm_logits,) + transformer_outputs[1:]
+            output = (bias_loss,) + output
+            # print(((lm_loss,) + output))
+            return ((lm_loss,) + output) if lm_loss is not None else output
+
+        # print('LM loss, Debias loss')
+        # print(output[0], output[1])
+        mc_loss = None
+        mc_logits = None
+        return GPT2DoubleHeadsModelOutput(
+            loss=lm_loss,
+            mc_loss=mc_loss,
+            logits=lm_logits,
+            mc_logits=mc_logits,
+            past_key_values=transformer_outputs.past_key_values,
+            hidden_states=transformer_outputs.hidden_states,
+            attentions=transformer_outputs.attentions,
+        )
